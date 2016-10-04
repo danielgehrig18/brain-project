81431b76c2e938ff43c3a7d50274a89d145a4625
Daniel Gehrig <danielgehrig@daniels-macbook-pro-3.home>
2016-10-04 23:53:59 +0200

    deleted unnecessary files

--- loady.m
+++
@@ -1,7 +0,0 @@
-function [ im ] = loady( folder, index )
-%LOAD Summary of this function goes here
-%   Detailed explanation goes here
-    file_path = strcat('data/set_',folder,'/',folder,'_', num2str(index),'.nii');
-    im = double(nii_read_volume(file_path));
-end
-
