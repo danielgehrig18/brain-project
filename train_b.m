446b0598a58a1f06553335af8ee4a2d5870c8df6
Daniel Gehrig <danielgehrig@daniels-macbook-pro-3.home>
2016-10-07 21:09:19 +0200

    general commenting replaced pearson coefficient with coefficient of determination (see MATLAB)

--- train_b.m
+++ train_b.m
@@ -1,20 +1,24 @@
 function [ b, error, r_squared ] = train_b( x_folder, y_file, limit1, limit2 )
-%TRAIN_B Summary of this function goes here
-%   Detailed explanation goes here
+%TRAIN_B Trains a b parameter vector for linear regression with data from
+%the x_folder and targets from the y_file. limit1 and limit2 are parameters
+%for the feature_extract function.
 
+% load targets
 y = csvread(y_file);
+
+% generate #data_points x (#features+1) data matrix
 X = generate_X(x_folder, limit1, limit2);    
 
-%minimization of square error -> derivation w. resp. to beta = 0
+% minimization of square error -> derivation w. resp. to beta = 0
+% TODO: possibly regularize to avoid numerical instability
 b = inv(X'*X)*X'*y; 
 
-
+% total error for all datapoints
 error = (X*b-y)'*(X*b-y);
 
-rho_xx = corr(X);
-c = corr(X, y);
+% calculate coefficient of determination
+SStot = (length(y)-1)*var(y);
 
-r_squared = c'*rho_xx*c;
-
+r_squared = 1 - error/SStot;
 end
 
