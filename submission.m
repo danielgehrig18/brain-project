a25d72490c91a650a6ce35787c0831c19b8d0ef2
Daniel Gehrig <danielgehrig@daniels-macbook-pro-3.home>
2016-10-07 21:10:17 +0200

    general commenting wrote code that creates a .csv file with all the test data targets and puts it in data/ folder

--- submission.m
+++ submission.m
@@ -1,10 +1,22 @@
-function [ average_error ] = submission( b, file, limit1, limit2 )
-%   SUBMISSION Summary of this function goes here
-%   Detailed explanation goes here
-X = generate_X(validation_folder, limit1, limit2);
+function [ ] = submission( b, folder, file, limit1, limit2 )
+%   SUBMISSION Calculates the expected target values with b for test data and
+%   writes it into file
+
+% generate test data matrix. Has dimensions 
+% #test_data_points x (#features + 1)
+X = generate_X(folder, limit1, limit2);
 y = X * b;
+y_length = length(y);
 
-csvwrite(file,y,2,2);
+% writes calculated values into file.
+data_matrix = ([(1:y_length)',y]);
 
+% check if there is already a file with name 'submit.csv', if so delete it
+if exist(file, 'file') == 2
+    delete(file);
+end
+
+% write matrix to csv file
+dlmwrite(file,data_matrix,'delimiter',',','-append');
 end
 
