45110f7a31124c57a7cb39fc0e40a916646fe2c1
Max <goettgem@student.ethz.ch>
2016-10-11 15:07:44 +0200

    brainbrowser bugfix

--- testing/brain_browser.m
+++ testing/brain_browser.m
@@ -2,7 +2,7 @@
     V_1 = nii_read_volume('data/set_train/train_168.nii');   %Age: 18
     V_2 = nii_read_volume('data/set_train/train_114.nii');  %Age: 45
     V_3 = nii_read_volume('data/set_train/train_99.nii');   %Age: 70
-    V_4 = nii_read_volume('data/set_train/train_.nii');  %Age: 90
+    V_4 = nii_read_volume('data/set_train/train_163.nii');  %Age: 90
     i = 115;
                      imshow([squeeze(V_1(:,:,i)), squeeze(V_2(:,:,i));
                  squeeze(V_3(:,:,i)), squeeze(V_4(:,:,i))],[]);
