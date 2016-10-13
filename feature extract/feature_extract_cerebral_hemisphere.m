7fbad6fc8799f0d6e2b8b5f9bd92c8df60bcf65d
Max <goettgem@student.ethz.ch>
2016-10-13 23:46:44 +0200

    grey matter extraction hinzugefügt visualize brain: das nii-lesen aus der schleife genommen cerebral hemisphere feature extract optimiert

--- feature extract/feature_extract_cerebral_hemisphere.m
+++ feature extract/feature_extract_cerebral_hemisphere.m
@@ -2,24 +2,53 @@
 %FEATURE_EXTRACT_CEREBRAL_HEMISPHERE Summary of this function goes here
 %   Detailed explanation goes here
 clear all
-for threshold =  440:2:480 
-X = zeros(1,278);
-%threshold = 462;    % 0 to 1524 // optimal threshold 462 determined
-%through numerical iteration
 
+relativethreshold = 2.6;        %260% of the mean
+
+X = zeros(1,278);
+%threshold = 462;    % 0 to 1524 // optimal threshold 462 determined through numerical iteration
 
 parfor i=1:278
     path_name = ['data/set_train/train_'  num2str(i) '.nii'];
-
-
-
     im = double(nii_read_volume(path_name));
-    cerebral_hemisphere_tissue_volume = 0;
     sz = size(im);
-    nonzeros = 0;
+    meanintensities = zeros(1,sz(3)); %meanintensities for eacht considered slice
+    
+    %find mean intensities of images and a total mean
+    slices = 0;
 
+        for z=115:sz(3)   %area between top of brain and ventricles
+            meanintensity = 0;
+            for y=1:5:sz(2)
+                pixels = 0;
+                intensity = 0;
+                for x=1:5:sz(1)     
+                    if (im(x,y,z) ~= 0)
+                        intensity = intensity + im(x,y,z);    %take sample intensity
+                        pixels = pixels + 1;
+                        meanintensity = intensity/pixels;                     
+                    end
+                end
+            end 
+                    slices = slices+1;
+                    meanintensities(1,z) = meanintensity; %vector of mean intensities in % of scale
+        end
+totalmeanintensity = 0;
+slices = 0;
+for j=1:176
+    if (meanintensities(1,j)~=0)
+        totalmeanintensity = totalmeanintensity + meanintensities(1,j);
+        slices = slices+1;
+    end
+end
+    totalmeanintensity = totalmeanintensity / slices;
+    
+    %find cerebrespinal fluid volume
+    cerebral_hemisphere_tissue_volume = 0;
+    nonzeros = 0;
     for z=115:sz(3)   %area between top of brain and ventricles
         area = 0;
+        threshold = meanintensities(1,z)*relativethreshold;
         for y=1:sz(2)
             for x=1:sz(1)     
                 if (im(x,y,z) ~= 0)
@@ -39,41 +68,33 @@ parfor i=1:278
 end
 
 Y = csvread('data/targets.csv');
-
 scatter(Y,X);
-
 hold on;
 
 y_dash = mean(Y);
 x_dash = mean(X);
 
-sz = size(Y);
 Sxx = 0;
 Syy = 0;
 Sxy = 0;
 
-yCalc = zeros(1,sz(1));
+yCalc = zeros(1,278);
 Error = 0;
 
-for i=1:sz(1)
-    Sxx = Sxx + (X(1,i) - x_dash)^2;
-    Syy = Syy + (Y(i,1) - y_dash)^2;
-    Sxy = Sxy + (X(1,i) - x_dash)*(Y(i,1) - y_dash);
+for j=1:278
+    Sxx = Sxx + (X(1,j) - x_dash)^2;
+    Syy = Syy + (Y(j,1) - y_dash)^2;
+    Sxy = Sxy + (X(1,j) - x_dash)*(Y(j,1) - y_dash);
 end
 
 m = Sxy/Sxx;
 b = y_dash - m*x_dash;
 
-for i=1:sz(1)
-    yCalc(1,i) = m * X(1,i) + b;
-    Error = Error + (yCalc(1,i) - Y(i,1))^2;
+for j=1:278
+    yCalc(1,j) = m * X(1,j) + b;
+    Error = Error + (yCalc(1,j) - Y(j,1))^2;
 end
 
-disp(['The MEAN-Error is: ' num2str(sqrt(Error/278)) ' years @ Threshold: ' num2str(threshold)]);
-
+disp(['The MEAN-Error is: ' num2str(sqrt(Error/278)) ' years @ Threshold: ' num2str(relativethreshold*100) '%']);
 
 plot(yCalc,X)
-hold off
-end
-
-
