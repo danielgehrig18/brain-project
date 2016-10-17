549b9e03869c4849748dffa0cadb17badd7400b5
Daniel Gehrig <danielgehrig@daniels-macbook-pro-3.home>
2016-10-17 23:39:06 +0200

    bug fixes and cleaning

--- testing/test_feature.m
+++ testing/test_feature.m
@@ -1,21 +1,18 @@
 addpath('data','testing','ReadData3D_version1k/nii','feature extract');
 
 % test feature
-X = generate_X('data/set_train', 720, 810);
-Y = generate_X('data/set_train', 500, 800);
+
+parameters = [th, sigma, 26];
+X = generate_X('data/set_train', 'feature_extract7', parameters);
 
 %%
 
 y = csvread('data/targets.csv');
 
 ax1 = subplot(2,1,1);
-scatter(ax1,X(:,1)./X(:,2),y)
+scatter(ax1,X(:,1),y)
 
-ax2 = subplot(2,1,2);
+%ax2 = subplot(2,1,2);
 % scatter(ax2,Y(:,2)./Y(:,1),y,'filled','d')
-scatter(ax2,X(:,2),y,'filled','d')
-
-
-    
-        
+%scatter(ax2,X(:,2),y,'filled','d')    
 
