b2bffbb2a2393bd4965c8cc2896e43a37b09cd01
Unknown <daniel.gehrig1@gmx.ch>
2016-10-25 14:57:35 +0200

    several new features and pca in generate X

--- train_b.m
+++ train_b.m
@@ -1,4 +1,4 @@
-function [ model, X] = train_b( x_folder, y_file, fun, parameters )
+function [ model, X, c] = train_b( x_folder, y_file, fun, parameters )
 %TRAIN_B Trains a b parameter vector for linear regression with data from
 %the x_folder and targets from the y_file. limit1 and limit2 are parameters
 %for the feature_extract function.
@@ -7,7 +7,11 @@ function [ model, X] = train_b( x_folder, y_file, fun, parameters )
 y = csvread(y_file);
 
 % generate #data_points x (#features+1) data matrix
-X = generate_X(x_folder, fun, parameters);    
+X = generate_X(x_folder, fun, parameters); 
+
+c = pca([X,y]);
+x_new = [X,y]*c;
+X = x_new(:,1:parameters.cutoff);
 
 % create linear model
 model = LinearModel.fit(X, y, 'RobustOpts', 'off');
