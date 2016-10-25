7e8f21e9d9a7f0929ed4dc2b36b03b17bfe26a50
Unknown <daniel.gehrig1@gmx.ch>
2016-10-25 15:00:12 +0200

    optimization

--- main.m
+++ main.m
@@ -6,14 +6,17 @@
 % add relevant folder to path
 addpath('feature extract', 'preprocess','ReadData3D_version1k/nii');
 
-% choose function and its parameters
-fun = 'feature_extract3';
-parameters = 10;
-% train b with linear regression model and parameters
-[model, X] = train_b('data/set_train', 'data/targets.csv', fun, parameters);
+%% choose function1 and its parameters
+fun1 = 'feature_extract10';
+parameters1 = struct('cutoff', 4, ...
+                    'bins', 2000);
+[model1, X1, c1] = train_b('data/set_train', 'data/targets.csv', fun1, parameters1);                    
+                
+
+%% train b with linear regression model and parameters
 disp('Training finished successfully!');
 
 disp('Creating submission file using Data: data/set_test and Targets: data/submit.csv ...');
 % submit target values for test set 
-y_hat = submission('data/set_test', 'data/submit.csv', model, fun, parameters);
+submission('data/set_test', 'data/submit.csv', {model1}, {fun1}, {parameters1}, c1);
 disp('Submission file created successfully!');
