5eaec0f92aaf2eeae060cf41f3d7181187dabe1f
Adrian <rucklia@student.ethz.ch>
2016-10-26 18:36:47 +0200

    testing stuff

--- testing/test_feature.m
+++ testing/test_feature.m
@@ -1,9 +1,9 @@
-addpath('data','testing','ReadData3D_version1k/nii','feature extract');
+addpath('../data','../testing','../ReadData3D_version1k/nii','../feature extract');
 
 % test feature
-parameters = struct('band', 5, ...
-                    'bins', 2000);
-X = generate_X('data/set_train', 'feature_extract11', parameters);
+parameters = struct('cgone',220,'rangeone',50,'cgtwo',750,'rangetwo',35,...
+                    'cgthree',1450,'rangethree',110);
+X = generate_X('data/set_train', 'feature_extract_3peaks_v1', parameters);
 
 %%
 close all
@@ -11,7 +11,7 @@ y = csvread('data/targets.csv');
 
 ax1 = subplot(2,1,1);
 
-scatter(ax1,y,X(:,3))
+scatter(ax1,y,X(:,1))
 %ax2 = subplot(2,1,2);
 % scatter(ax2,Y(:,2)./Y(:,1),y,'filled','d')
 %scatter(ax2,y,X(:,2),'filled','d')
