37b9b8bfeb991a356fe9d85b4ce578e4c90d2d5c
Unknown <daniel.gehrig1@gmx.ch>
2016-10-28 11:58:35 +0200

    score 50.81

--- train_b.m
+++ train_b.m
@@ -9,7 +9,11 @@ y = csvread(y_file);
 % generate #data_points x (#features+1) data matrix
 X = generate_X(x_folder, fun, parameters); 
 
+% determine weights
+w = histcounts(y, 1:100)';
+w(w==0)=1;
+w = 2*w(y);
+
 % create linear model
-model = LinearModel.fit(X, y, 'RobustOpts', 'off');
-end
+model = LinearModel.fit(X, y, 'RobustOpts', 'on', 'Weights',w);
 
