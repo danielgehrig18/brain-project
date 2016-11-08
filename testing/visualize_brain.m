691e905153be43fe4e7179caa572dcb7ea256f9b
Daniel Gehrig <danielgehrig@vpn-global-038-dhcp.ethz.ch>
2016-11-08 17:27:35 +0100

    wrote custom vis brain script which uses superpixels to segment image

--- testing/visualize_brain.m
+++ testing/visualize_brain.m
@@ -1,25 +1,32 @@
 % show brains
+% sick brains are:
+% 9, 14, 16, 17, 18, 28, 31, 37, 38, 49, 52, 55, 56, 59, 61, 66, 72, 78, 
+% 79, 97, 103, 109, 116, 118, 122, 126, 131, 133, 134, 136, 139, 144, 
+% 152, 157, 158, 159, 162, 163, 168, 176, 178, 179, 181, 182, 188, 189,
+% 191, 195, 197, 198, 203, 206, 207, 219, 221, 226, 230, 242, 243, 250, 
+% 256, 257, 258, 261, 262, 266, 267 
 
 close all
 
-V_1 = nii_read_volume('data/set_train/train_23.nii');   %Age: 18
-V_2 = nii_read_volume('data/set_train/train_172.nii');  %Age: 45
-V_3 = nii_read_volume('data/set_train/train_14.nii');   %Age: 70
-V_4 = nii_read_volume('data/set_train/train_118.nii');  %Age: 90
-for i=1:176
-    V_1 = mean(V_1, 1);
-    V_2 = mean(V_2, 1);
-    V_3 = mean(V_3, 1);
-    V_4 = mean(V_4, 1);
+V_1 = nii_read_volume('data/set_train/train_103.nii');   % sick
+V_2 = nii_read_volume('data/set_train/train_158.nii');  % sick
+V_3 = nii_read_volume('data/set_train/train_153.nii');   % not sick
+V_4 = nii_read_volume('data/set_train/train_208.nii');  % not sick
+i = 1;
+
+block = [V_1(:,:,i), V_2(:,:,i); V_3(:,:,i), V_4(:,:,i)];
+[L,NumLabels] = superpixels3(block,3);
+
+while true
+    l = L(:,:,i);
+    imshow(label2rgb(l));
     
-    V_1 = uint8(V_1 * 255 / max(V_1(:)));
-    V_2 = uint8(V_2 * 255/ max(V_2(:)));
-    V_3 = uint8(V_3 * 255/ max(V_3(:)));
-    V_4 = uint8(V_4 * 255/ max(V_4(:)));
-    
-    imshow([squeeze(V_1), squeeze(V_2);
-            squeeze(V_3), squeeze(V_4)],[]);
-   % pause(0.2)
+    %imshow(imrotate(block(:,:,i), 270),[]);
+    if waitforbuttonpress
+        i = i-2
+    else
+        i = i+2
+    end
 end
 
     
